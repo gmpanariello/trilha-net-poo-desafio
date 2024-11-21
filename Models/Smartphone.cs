5fa70d481d391fcbbef2cde796fa8233c3e6da5a
Giovanna M. Panariello <contatopanariello@gmail.com>
2024-11-21 15:40:04 -0300

    Resolução do Desafio do Projeto + Implementações extras (desligar, nome de quem liga)

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,16 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
@@ -16,9 +20,14 @@ namespace DesafioPOO.Models
             Console.WriteLine("Ligando...");
         }
 
-        public void ReceberLigacao()
+        public void Desligar()
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine("Desligando...");
+        }
+
+        public void ReceberLigacao(string nome)
+        {
+            Console.WriteLine($"Recebendo ligação de {nome}...");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "(00) 12345-6789", modelo: "Nokia 3200", imei: "111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo ("Facebook");
+nokia.ReceberLigacao("Maria");
+nokia.Desligar();
+
+Console.WriteLine ("\n");
+
+Console.WriteLine ("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "(99) 98765-4321", modelo: "Iphone 13 Pro Max", imei: "222222222", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao("João");
+iphone.InstalarAplicativo("Instagram");
+iphone.Desligar();
